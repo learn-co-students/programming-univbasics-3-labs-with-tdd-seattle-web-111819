337eb8acf0809e4345da7a01b4f2121d60276d69
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-02 21:16:29 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
 # Add your variables here
+
+first_number = 0
+
+second_number = Float 2.01
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
